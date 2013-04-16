1f0abf2ea524a8c64aeb0455b3d203fa59baff2a
Sanne Janssen <sanne.janssen@wunderkraut.com>
2013-04-16 21:38:56 +0200

    update ruby file

--- sites/all/themes/wundertheme/config.rb
+++ sites/all/themes/wundertheme/config.rb
@@ -1,8 +1,5 @@
-require 'susy'
 require 'toolkit'
-require 'respond-to'
 # Require any additional compass plugins here.
-
 
 # Set this to the root of your project when deployed:
 http_path = "/sites/all/themes/wundertheme/"
