0bfb0832801698a5d821353b49d1166a3a82aafc
Sanne Janssen <info@sannejanssen.be>
2013-05-14 23:16:43 +0200

    update theming

--- www/sites/all/themes/wundertheme/template.php
+++ www/sites/all/themes/wundertheme/template.php
@@ -129,7 +129,7 @@ function wundertheme_breadcrumb($variables) {
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-    $output .= '<div class="breadcrumb">' . implode(' > ', $breadcrumb) . '</div>';
+    $output .= '<div class="breadcrumb">' . implode('<span class="pipe">|</span>', $breadcrumb) . '</div>';
 
     return $output;
   }
